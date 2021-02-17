b55ab19e9f0a84b7f887a63e64c0d8189846c3d0
Fernando Valenzuela Ramos <amdati5000@gmail.com>
2021-02-16 23:00:03 -0600

    docs_agregado

---
+++ docs/precache-manifest.e9b634ded63264921588abafb25ace9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9113c8c694a48da3b382f549a2fcf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec9bceb186a053b0e971",
+    "url": "/static/css/main.6fad29db.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec9bceb186a053b0e971",
+    "url": "/static/js/main.d0fd50aa.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
